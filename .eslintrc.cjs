f71d3a975528c145a1bcc590e688c6b6c19f5b4f
Mariana-Diachun <99080483+Mariana-Diachun@users.noreply.github.com>
2023-02-17 12:14:28 +0100

    add prettier, eslint, husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'eslint-config-prettier', 'plugin:prettier/recommended'],
+  plagins: ['eslint-plugin-prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: { 'prettier/prettier': 'error' },
+};
