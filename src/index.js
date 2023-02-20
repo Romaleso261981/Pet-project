e8e0ebbb17b4c44abb1dfd09aea2aa77ccfaf9ab
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-02-20 12:30:09 +0300

    added several components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 // import { BrowserRouter } from 'react-router-dom';
 
-const basename = process.env.NODE_ENV !== 'production' ? '/final-project-team5-front' : '/final-project-team5-front/';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const basename = process.env.NODE_ENV !== 'production' ? '/final-project-team5-front' : '/final-project-team5-front/';
+const basename = "localhost:3001/";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 console.log(basename);
 root.render(
   <React.StrictMode>
---
+++ src/services/reportsApi.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+// type === income OR expense
+export async function getTransactionsSummaryByYear(year, type) {
+  const { data } = await axios.get(`/report/summaryByMonth/${year}/${type}`);
+  return data;
+}
+
+export async function getTransactionsByDate(month, year) {
+  const { data } = await axios.get(`/report/byCategory/${month}/${year}`);
+  return data;
+}
+
+export async function getTransactionsByMonth(month, year) {
+  const { data } = await axios.get(`/report/byCategory/${month}/${year}`);
+  return data;
+}
+
+export async function getTransactionsByCategory(category, month, year) {
+  const { data } = await axios.get(
+    `/report/amountByDescription/${category}/${month}/${year}`
+  );
+  return data;
+}
