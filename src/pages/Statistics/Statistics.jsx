6b1e55c7490c2ecd4f8fd7a5d6799bae871a2aa6
Silard Hudan <silard.gudan@gmail.com>
2023-02-22 21:41:09 +0200

    fix icons

--- src/pages/Statistics/Statistics.jsx
+++ src/pages/Statistics/Statistics.jsx
@@ -1,12 +1,13 @@
 import { Background } from "../../components/Background/Background";
 import styled from "./Statistics.module.scss";
-
+import { CurrentMonth } from "../../components/Report/CurrentMonth";
 const Statistics = () => {
   return (
     <>
       <Background />
       <div className={styled.container}>
         <div className={styled.wrapper}></div>
+        <CurrentMonth />
       </div>
     </>
   );
