80001d282501ba7e6e0a239c94052792ed9f4a9e
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-02-24 14:49:18 +0300

    delete background

--- src/pages/WalletPage/Wallet.jsx
+++ src/pages/WalletPage/Wallet.jsx
@@ -1,13 +1,15 @@
-import { Background } from "../../components/Background/Background";
 import { Transaction } from "components/Transaction/Transaction";
 import TableTransaction from "components/TableTransaction/TableTransaction";
 import TableSummary from "components/TableSummary/TableSummary";
+import { AnimatedCabbagesBg } from "../../components/AnimatedBg/AnimatedBg";
+import { AnimatedBottomCabbages } from "../../components/AnimatedCabbagesBottom/AnimatedCabbagesBottom";
 import styled from "./Wallet.module.scss";
 
 const Wallet = () => {
   return (
     <>
-      <Background />
+      <AnimatedCabbagesBg />
+      <AnimatedBottomCabbages />
       <div className={styled.container}>
         <div className={styled.wrapper}>
           <div className={styled.transaction_all}>
