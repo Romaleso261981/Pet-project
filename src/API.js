eccd8b577c8c2e206de511eddd64baeda0102108
Ihor Borkaliuk <happyviolence1990@gmail.com>
2023-02-26 17:49:04 +0100

    small fixes

--- src/API.js
+++ src/API.js
@@ -5,12 +5,30 @@ const API = axios.create({
 });
 
 const authToken = {
-  set(accessToken) {
-    API.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+  set(token) {
+    API.defaults.headers.common.Authorization = `Bearer ${token}`;
   },
 
   unset() {
     API.defaults.headers.common.Authorization = ``;
   },
 };
+
+API.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response.status == 401) {
+      const refreshToken = localStorage.getItem("refreshToken");
+      try {
+        const { data } = await API.post("auth/users/refresh", { refreshToken });
+        authToken.set(data.accessToken);
+        localStorage.setItem("refreshToken", data.refreshToken);
+        return API(error.config);
+      } catch (error) {
+        return Promise.reject(error);   
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 export { API, authToken };
