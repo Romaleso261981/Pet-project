b93235d0db8193fc121f55ec5e3b542c9721b6b1
Silard Hudan <silard.gudan@gmail.com>
2023-02-28 18:57:02 +0200

    fix css on report page

--- src/pages/ReportsPage/ReportsPage.jsx
+++ src/pages/ReportsPage/ReportsPage.jsx
@@ -2,30 +2,21 @@ import Reports from "components/Reports/Reports";
 import ReportsBalance from "components/ReportsBalance/ReportsBalance";
 import styled from "./ReportsPage.module.scss";
 
-// import IncomeForm from 'components/IncomeForm/IncomeForm';
-// import CategoryList from '../../components/CategoryList/CategoryList';
-// import ChartCategory from '../../components/ChartCategory/ChartCategory';
-// import ChartCategoryMobile from '../../components/ChartCategoryMobile/ChartCategoryMobile';
-// import { useMediaQuery } from 'react-responsive';
+
 
 const ReportsPage = () => {
-  // const isMobile = useMediaQuery({
-  //   query: "(min-width: 767px)",
-  // });
+
   return (
     <>
       <div className={styled.container}>
-        <div>
-          <ReportsBalance />
+        <ReportsBalance />
+        <div className={styled.wrapper}>
+          <Reports />
         </div>
         
-        <Reports />
       </div>
 
-      {/* <CategoryList /> */}
-      {/* <h1>CategoryList</h1>
-      {true ? <h1>ChartCategory</h1> : <h1>ChartCategoryMobile</h1>} */}
-      {/* {isMobile ? <ChartCategory /> : <ChartCategoryMobile />} */}
+     
     </>
   );
 };
