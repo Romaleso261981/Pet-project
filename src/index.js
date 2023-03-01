50237dc105db956fa9af5c17e2eced798b04bc68
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-03-01 16:18:00 +0300

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -7,13 +7,13 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
-// const basename = process.env.NODE_ENV !== "production" ? "/" : "/";
+const basename = process.env.NODE_ENV !== "production" ? "/" : "kapusta-team5.netlify.app";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={"/"}>
+        <BrowserRouter basename={basename}>
           <App />
         </BrowserRouter>
       </PersistGate>
