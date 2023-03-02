c9f07ae5a096af0a23c93ae3f318466825224ec5
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-03-02 14:13:29 +0300

    Update Calc.jsx

--- src/components/TransactionForm/Calc/Calc.jsx
+++ src/components/TransactionForm/Calc/Calc.jsx
@@ -1,32 +1,35 @@
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import { FaCalculator } from "react-icons/fa";
 import styled from "./Calc.module.scss";
 
 export function Calc({ form, field }) {
-    const { name, value } = field;
-    const { setFieldValue } = form;
+  const { name, value } = field;
+  const { setFieldValue } = form;
 
-    const handleChange = (e) => {
-        setFieldValue(name, e.target.value);
-    };
+  const handleChange = (e) => {
+    setFieldValue(name, e.target.value);
+  };
 
-    return (
-        <div className={styled.wrapper}>
-            <div className={styled.wrapper__input}>
-                <TextField
-                    required
-                    name={name}
-                    value={value}
-                    onChange={handleChange}
-                    placeholder="00.00 UAH"
-                    inputProps={{
-                        pattern: "^[1-9]*[.]?[0-9]{1,2}$"
-                    }}
-                />
-            </div>
-            <div className={styled.wrapper__icon}>
-                <FaCalculator />
-            </div>
-        </div>
-    )
+  return (
+    <div className={styled.wrapper}>
+      <div className={styled.wrapper__input}>
+        <TextField
+          required
+          name={name}
+          value={value}
+          onChange={handleChange}
+          placeholder="00.00 UAH"
+          min="00.00"
+          max="10000000.00"
+          step="0.1"
+          // inputProps={{
+          //     pattern: "^[1-9]*[.]?[0-9]{1,2}$"
+          // }}
+        />
+      </div>
+      <div className={styled.wrapper__icon}>
+        <FaCalculator />
+      </div>
+    </div>
+  );
 }
