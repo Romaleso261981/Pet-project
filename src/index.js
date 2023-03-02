56a127ba173ac75c8c0264e808b288c50f300f12
Romaleso261981 <98737355+Romaleso261981@users.noreply.github.com>
2023-03-02 11:40:32 +0300

    fix base url

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { BrowserRouter } from "react-router-dom";
 
-const basename = process.env.NODE_ENV !== "production" ? "/" : "kapusta-team5.netlify.app";
+const basename = process.env.NODE_ENV !== "production" ? "/" : "/";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
